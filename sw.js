8ceb6d2b4dc48dcc10bb6cde3e07b1cc8b744bbd
AyushSehgal <ayushs25@berkeley.edu>
2020-08-21 14:31:34 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c0eeca0188c206873672.js"
+    "url": "webpack-runtime-e0953b63101b239633ed.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "839d1cee200ec5908ce46bc57cd390c6"
+    "revision": "7533e9f2931cb775b6bbf12745d10ef7"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "759b5ccc167b7852904e9e85c5f3015d"
+    "revision": "0ea437f79a75bb9fd72a805483e859f9"
   },
   {
     "url": "polyfill-d06486d78b0d3a46b835.js"
